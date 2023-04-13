47e0ec4667e4ef1b907b5ef72b56d5088b7b3a77
Rachel Ng <rachel.ng.135@gmail.com>
2023-04-13 13:24:14 -0400

    google links

---
+++ google/README.md
@@ -0,0 +1,3 @@
+# Google 
+
+based off [Google Links](https://arcboosts.com/boosts/11/google-links) boost
---
+++ google/content.js
@@ -0,0 +1,87 @@
+const q = queryParams().q.replace(/\+/g, ' ');
+let query = queryParams().q.replace(/\+/g, ' ');
+
+const nav = document.createElement('div');
+document.querySelector('.nfdoRb').parentElement.appendChild(nav);
+nav.classList.add('nfdoRb');
+nav.classList.add('nnav');
+
+console.log(`[QUERY]\t${query}`);
+
+if (q.includes("site:")) {
+  query_tokens = query.split(" ");
+  qs = query_tokens.filter(q => !q.includes("site:"));  
+  query = qs.join(" ");
+  console.log(`query\t${query}`);
+}
+
+if (!q.includes("site:wikipedia.org")) {
+  const wikipediaLink = 'https://www.google.com/search?q=' + encodeURIComponent("site:wikipedia.org " + query);
+  addLink(imgIcon('https://en.wikipedia.org/static/favicon/wikipedia.ico'), 'Wikipedia', wikipediaLink);
+}
+
+if (!q.includes("site:reddit.com")) {
+  const redditLink = 'https://www.google.com/search?q=' + encodeURIComponent("site:reddit.com " + query);
+  addLink(imgIcon('https://www.redditstatic.com/desktop2x/img/favicon/android-icon-192x192.png'), 'Reddit', redditLink);
+}
+
+if (q.includes("site:")) {
+  const googleLink = 'https://www.google.com/search?q=' + encodeURIComponent(query);
+  addLink(imgIcon('https://www.google.com/favicon.ico'), 'Google', googleLink);
+}
+
+
+function queryParams() {
+  var query = location.search.substr(1);
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
+}
+
+function createLink(icon, name, url) {
+  const a = document.createElement('a');
+  a.setAttribute('href', url);
+  a.classList.add('zItAnd');
+  a.classList.add('FOU1zf');
+  a.classList.add('GMT2kb');
+
+  const d = document.createElement('div');
+  d.classList.add('O3S9Rb');
+  d.append(name);
+
+  a.append(icon);
+  a.append(d);
+  
+  return a;
+}
+
+function textIcon(symbol) {
+  const icon = document.createElement('span');
+  icon.innerText = symbol;
+  icon.style.marginRight = '0.5em';
+  icon.style.fontWeight = 'bold';
+
+  return icon;
+}
+
+function imgIcon(img) {
+  const icon = document.createElement('img');
+  icon.src = img;
+  icon.classList.add('mUKzod');
+  icon.setAttribute("aria-hidden", "true");
+  icon.setAttribute("focusable", "false");
+
+  return icon;
+}
+
+function addLink(symbol, name, url) {
+  const link = createLink(symbol, name, url);
+  console.log(link);
+  console.log(document.querySelector('.nfdoRb'));
+  // document.querySelector('.nfdoRb').appendChild(link);
+  nav.appendChild(link);
+}
+
