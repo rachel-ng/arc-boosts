28c6e0c01288fff874e35958b8fd86aadc3ab43c
Rachel Ng <rachel.ng.135@gmail.com>
2024-09-17 15:56:18 -0400

    google calendar

--- gcal/content.js
+++ gcal/content.js
@@ -20,19 +20,18 @@ observer.observe(document, {
 function toggle_calendar(calendar="Work", debug=false) {
   if (debug) { console.log(window.location.href) }
   if (window.location.href.indexOf("month") > -1) {
-    let work = [...[...document.querySelector("div[aria-label='My\ calendars']").childNodes].filter(c => c.innerText == calendar)[0].childNodes][0].querySelector("div.lcPUt").querySelector("input")
-    if (debug) { console.log("month"); console.log(work);}
-    if (work.checked) {
-      work.click();
+    let cal = [...[...document.querySelector("div[aria-label='My\ calendars']").childNodes].filter(c => c.innerText == calendar)[0].childNodes][0].querySelector("div.lcPUt").querySelector("input")
+    if (debug) { console.log("month"); console.log(cal);}
+    if (cal.checked) {
+      cal.click();
     }
   }
 
   if (window.location.href.indexOf("week") > -1) {
-    let work = [...[...document.querySelector("div[aria-label='My\ calendars']").childNodes].filter(c => c.innerText == calendar)[0].childNodes][0].querySelector("div.lcPUt").querySelector("input")
-    if (debug) { console.log("week"); console.log(work); }
-    if (!work.checked) {
-      work.click();
+    let cal = [...[...document.querySelector("div[aria-label='My\ calendars']").childNodes].filter(c => c.innerText == calendar)[0].childNodes][0].querySelector("div.lcPUt").querySelector("input")
+    if (debug) { console.log("week"); console.log(cal); }
+    if (!cal.checked) {
+      cal.click();
     }
   }
 }
-
